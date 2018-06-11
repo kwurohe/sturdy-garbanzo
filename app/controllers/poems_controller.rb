a3bab17b9bd72191738820c8b18c9225a3f9b7d7
Keren WuRohe <kerenrohe@gmail.com>
2018-06-10 20:55:09 -0400

    Quiz 2 final

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,11 +1,19 @@
 class PoemsController < ApplicationController
+  def index
+    @poem = Poem.order("RANDOM()").first
+  end
+  
   def new
     @poem = Poem.new
   end
   
   def create
     @poem = Poem.create(poem_params)
-    redirect_to root_path
+    redirect_to poem_path(@poem)
+  end
+  
+  def show
+    @poem = Poem.find(params[:id])
   end
   
   private
